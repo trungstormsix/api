fdf77f92929e46ae304f9c19d8729ac6f2b9c13a
oCoder\TrungTruong <trungstormsix@gmail.com>
2016-10-21 09:55:03 +0700

    Change name of Permission to singular

---
+++ app/Http/Controllers/Admin/PermissionController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Models\Permission;
+
+class PermissionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $permissions = Permission::all();
+        return view('admin.permissions.home',['permissions' => $permissions]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.permissions.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $post_data = $request->all();
+        $permissions = new Permission;
+        $permissions->name = $post_data['name'];
+        $permissions->display_name = $post_data['display_name'];
+        $permissions->description = $post_data['description'];
+        $permissions->save();
+        return redirect('admin/user/permissions');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $permissions = Permission::findOrFail($id);
+        return view('admin.permissions.edit',compact('permissions'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+       
+        $permissions = Permission::findOrFail($id); 
+        if(!$permissions) return redirect('admin/user/permissions');
+        $permissions->update($request->all()); 
+        return redirect('admin/user/permission/edit/'.$id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function delete($id)
+    {
+        $permissions = Permission::find($id);
+        $permissions->delete();
+        return redirect('permissions');
+    }
+
+    
+}
---
+++ app/Models/Permission.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Zizaco\Entrust\EntrustPermission;
+
+class Permission extends EntrustPermission
+{
+	protected $fillable =  ['name', 'display_name', 'description', 'created_at', 'updated_at'];
+
+    var $table = 'permissions';
+    /**
+     * The videos that belong to the playlist.
+     */
+    public function Roles(){
+    	return $this->belongsToMany('App\Models\Roles', 'permission_role');
+    }
+}
